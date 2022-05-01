aa0b7661a18071fda1f2723c31ca77a463583119
Naiizey <79869581+Naiizey@users.noreply.github.com>
2022-05-01 22:49:32 +0200

    Update dut_info_partie1.sql

--- dut_info_partie1.sql
+++ dut_info_partie1.sql
@@ -14,8 +14,6 @@ CREATE TABLE _individu(
     nationalite varchar(32) not null,
     INE varchar(32) not null,
     CONSTRAINT _individu_pk PRIMARY KEY (id_individu),
-    no_candidat int,
-    code_nip varchar(32)
 );
 
 CREATE TABLE _etudiant(
@@ -28,9 +26,7 @@ CREATE TABLE _etudiant(
     domainante_bac varchar(32) not null,
     special_bac varchar(32) not null,
     mois_annee_obtention_bac char(7),
-    CONSTRAINT _etudiant_pk PRIMARY KEY (code_nip),
-    id_individu integer not null,
-    id_semestre int not null
+    CONSTRAINT _etudiant_pk PRIMARY KEY (code_nip)
 );
 
 CREATE TABLE _candidat(
@@ -48,22 +44,22 @@ CREATE TABLE _candidat(
     specialite_prec varchar(50) not null,
     LV1 varchar(50) not null,
     LV2 varchar(50) not null,
-    CONSTRAINT _candidat_pk PRIMARY KEY (no_candidat),
-    id_individu integer not null,
-    code_nip varchar(32) not null
+    CONSTRAINT _candidat_pk PRIMARY KEY (no_candidat)
 );
 
 CREATE TABLE _inscription(
     groupe_tp char(2) not null,
-    amenagement_evaluation varchar(50) not null
+    amenagement_evaluation varchar(50) not null,
+    code_nip varchar(32),
+    id_semestre int,
+    CONSTRAINT _inscription_pk PRIMARY KEY (code_nip, id_semestre)
 );
 
 CREATE TABLE _semestre(
     id_semestre int,
     num_semestre char(5) not null,
     annee_univ char(9) not null,
-    CONSTRAINT _semestre_pk PRIMARY KEY (id_semestre),
-    code_nip int not null
+    CONSTRAINT _semestre_pk PRIMARY KEY (id_semestre)
 );
 
 CREATE TABLE _module(
@@ -73,15 +69,56 @@ CREATE TABLE _module(
     CONSTRAINT _module_pk PRIMARY KEY (id_module)
 );
 
-ALTER TABLE _candidat ADD
-    CONSTRAINT _candidat_id_individu_fk FOREIGN KEY (id_individu) REFERENCES _individu(id_individu);
+CREATE TABLE _resultat{
+    moyenne double not null,
+    id_module char(5),
+    code_nip varchar(32),
+    id_semestre int,
+    CONSTRAINT _resultat_pk PRIMARY KEY (id_module, code_nip, id_semestre)
+};
 
-ALTER TABLE _individu ADD
-    CONSTRAINT _individu_no_candidat_fk FOREIGN KEY (no_candidat) REFERENCES _candidat(no_candidat);
-ALTER TABLE _candidat ADD
-    CONSTRAINT _individu_code_nip_fk FOREIGN KEY (code_nip) REFERENCES _etudiant(code_nip);
+CREATE TABLE _programme{
+    coefficient double not null,
+    id_module char(5),
+    id_semestre int,
+    CONSTRAINT _programme_pk PRIMARY KEY (id_module, id_semestre)
+};
 
-ALTER TABLE _etudiant ADD
-    CONSTRAINT _etudiant_id_individu_fk FOREIGN KEY (id_individu) REFERENCES _individu(id_individu);
-ALTER TABLE _etudiant ADD
-    CONSTRAINT _etudiant_id_semestre_fk FOREIGN KEY (id_semestre) REFERENCES _semestre(id_semestre);
+CREATE TABLE _postuler{
+    id_individu int,
+    no_candidat int,
+    CONSTRAINT _postuler_pk PRIMARY KEY (id_individu, no_candidat)
+};
+
+CREATE TABLE _s_inscrire{
+    id_individu int,
+    code_nip varchar(32),
+    CONSTRAINT _s_inscrire_pk PRIMARY KEY (id_individu, code_nip)
+};
+
+ALTER TABLE _resultat ADD
+    CONSTRAINT _resultat_id_module_fk FOREIGN KEY (id_module) REFERENCES _module(id_module);
+ALTER TABLE _resultat ADD
+    CONSTRAINT _resultat_code_nip_fk FOREIGN KEY (code_nip) REFERENCES _etudiant(code_nip);
+ALTER TABLE _resultat ADD
+    CONSTRAINT _resultat_id_semestre_fk FOREIGN KEY (id_semestre) REFERENCES _semestre(id_semestre);
+    
+ALTER TABLE _programme ADD
+    CONSTRAINT _programme_id_module_fk FOREIGN KEY (id_module) REFERENCES _module(id_module);
+ALTER TABLE _programme ADD
+    CONSTRAINT _programme_id_semestre_fk FOREIGN KEY (id_semestre) REFERENCES _semestre(id_semestre); 
+
+ALTER TABLE _inscription ADD
+    CONSTRAINT _inscription_code_nip_fk FOREIGN KEY (code_nip) REFERENCES _etudiant(code_nip);
+ALTER TABLE _inscription ADD
+    CONSTRAINT _inscription_id_semestre_fk FOREIGN KEY (id_semestre) REFERENCES _semestre(id_semestre);
+
+ALTER TABLE _postuler ADD
+    CONSTRAINT _postuler_id_individu_fk FOREIGN KEY (id_individu) REFERENCES _individu(id_individu);
+ALTER TABLE _postuler ADD
+    CONSTRAINT _postuler_no_candidat_fk FOREIGN KEY (no_candidat) REFERENCES _candidat(no_candidat);
+    
+ALTER TABLE _s_inscrire ADD
+    CONSTRAINT _s_inscrire_id_individu_fk FOREIGN KEY (id_individu) REFERENCES _individu(id_individu);
+ALTER TABLE _s_inscrire ADD
+    CONSTRAINT _s_inscrire_code_nip_fk FOREIGN KEY (code_nip) REFERENCES _etudiant(code_nip);
