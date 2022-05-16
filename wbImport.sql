eef5d95071f75e4c80d7cec489c12c8ba45a33c2
Naiizey <79869581+Naiizey@users.noreply.github.com>
2022-05-16 10:50:33 +0200

    Update wbImport.sql

--- wbImport.sql
+++ wbImport.sql
@@ -19,9 +19,8 @@ wbImport -file=/media/flguillou/SSD de Flo/Cours/SAE 2.04/data/v_inscriptions.cs
          -filecolumns=%wb_skip%,%wb_skip%,%wb_skip%,%wb_skip%,%wb_skip%,ine,nom,prenom,date_naissance,sexe,nationalite,code_postal,ville
          -dateformat="yyyy-MM-dd";
 
-WbImport -file=./data/ppn.csv
+WbImport -file=/media/flguillou/SSD de Flo/Cours/SAE 2.04/data/ppn.csv
          -header=true
          -delimiter=';'
          -table=_module
-         -schema=partie1
          -filecolumns=id_module,ue,libelle_module;
